ef562a55a0231319d3d3744f39700a8fe6aaf84f
brunocd2 <bruno.santos@cd2.com.br>
2023-03-06 17:16:34 -0300

    remove

--- src/App.js
+++ src/App.js
@@ -4,9 +4,7 @@ import Routes from "./routes";
 export default function App() {
   return (
     <GlobalProvider>
-     
-<script type="text/javascript" src="//cdn.iubenda.com/cs/iubenda_cs.js" charset="UTF-8" async></script>
-      <div className="container">
+           <div className="container">
         <Routes />
       </div>
     </GlobalProvider>
--- src/index.js
+++ src/index.js
@@ -7,7 +7,13 @@ import { GlobalStyle } from './styles/globalStyles';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <script type="text/javascript" src="//cdn.iubenda.com/cs/iubenda_cs.js" charset="UTF-8" async></script>
+
     <GlobalStyle />
+    <script type="text/javascript" src="//cdn.iubenda.com/cs/iubenda_cs.js" charset="UTF-8" async></script>
+
     <App />
+    <script type="text/javascript" src="//cdn.iubenda.com/cs/iubenda_cs.js" charset="UTF-8" async></script>
+
   </React.StrictMode>
 );
