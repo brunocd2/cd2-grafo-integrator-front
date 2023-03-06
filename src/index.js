d532b6e2fa827183f372f8fec6d2d928a433da91
brunocd2 <bruno.santos@cd2.com.br>
2023-03-06 17:28:11 -0300

    adjust new version

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import { GlobalStyle } from './styles/globalStyles';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    
     <script type="text/javascript" src="//cdn.iubenda.com/cs/iubenda_cs.js" charset="UTF-8" async></script>
 
     <GlobalStyle />
