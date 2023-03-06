56a77969dd846d17cc431fb84fc69d860f59d344
Jeziel Marques <jezielmarques7@gmail.com>
2023-03-06 14:32:13 -0300

    feat(first-access): create screen

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes as RoutesDom, Route } from "react-router-dom";
 
 import Menu from "./components/Menu";
 import Dashboard from "./pages/Dashboard";
+import FirstAccess from "./pages/FirstAccess";
 import ForgotPassword from "./pages/ForgotPassword";
 import Login from "./pages/Login";
 import Notifications from "./pages/Notifications";
@@ -11,14 +12,15 @@ import Products from "./pages/Products";
 export default function Routes() {
   return(
     <BrowserRouter>
-      <Menu hideRoutes={["/", "/nova-senha"]}/>
-
+      <Menu hideRoutes={["/", "/nova-senha", "/primeiro-acesso"]}/>
       <RoutesDom>
         <Route path="/" element={<Login />} />
         <Route path="/produtos-cadastrados/:filterType?/:filter?" element={<Products />} />        
         <Route path="/notificacoes" element={<Notifications />} />        
         <Route path="/dashboard" element={<Dashboard />} />        
         <Route path="/nova-senha" element={<ForgotPassword />} />        
+        <Route path="/primeiro-acesso" element={<FirstAccess />} />        
+
       </RoutesDom>
     </BrowserRouter>
   )
